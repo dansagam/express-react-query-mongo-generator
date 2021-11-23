c6d9e0f896d59a1427a80154702ba71ad04bcc29
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-23 14:57:23 +0100

    server side created

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 import express from 'express'
 import { config } from 'dotenv'
+import path from 'path'
+import morgan from 'morgan'
+import connectDB from './db/connectDB.js'
 config({ silent: true })
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+connectDB()
+if (process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '/client/build')))
+
+   app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   )
+} else {
+   app.get('/', (req, res) => {
+      res.send('API is running')
+   })
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(
+   PORT, () =>
+   console.log(`server running in  ${process.env.NODE_ENV} mode on port ${PORT} 🔥 !`)
+)
