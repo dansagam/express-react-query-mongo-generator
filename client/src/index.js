6e2e3a33b3a49adc091f68e981396686b462491d
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-25 13:04:15 +0100

    install the material UI

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import store from './app/store'
+import { store } from './app/store'
 
 ReactDOM.render(
    <React.StrictMode>
       <Provider store={store}>
-         <App />
+         <Router>
+            <App />
+         </Router>
       </Provider>
    </React.StrictMode>,
    document.getElementById('root')
