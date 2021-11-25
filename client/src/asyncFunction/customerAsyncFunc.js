84de225c5b2257e68291b8628350cde0dbc3b841
dansagam <olukayodeogunnowo@yahoo.com>
2021-11-25 13:59:16 +0100

    create the action creator for customer payload

---
+++ client/src/asyncFunction/customerAsyncFunc.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const getCustomersFunc = async () => {
+   const response = await axios.get('/api/v2/customers')
+   return response
+}
+export const getCustomerByIdFunc = async (id) => {
+   const response = await axios.get(`/api/v2/customers/${id}`)
+   return response
+}
+export const addCustomerFunc = async (newData, config) => {
+   const response = await axios.post(`/api/v2/customers`, newData, config)
+   return response
+}
+
+export const updateCustomerFunc = async (id, newData, config) => {
+   const response = await axios.put(`/api/v2/customers/${id}`, newData, config)
+   return response
+}
+
+export const deleteCustomerFunc = async (id) => {
+   const response = await axios.delete(`/api/v2/customers/${id}`)
+   return response
+}
---
+++ client/src/reducers/AsyncSlice/customerAsync.js
@@ -0,0 +1,100 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import {
+   addCustomerFunc,
+   deleteCustomerFunc,
+   getCustomerByIdFunc,
+   getCustomersFunc,
+   updateCustomerFunc
+} from "../../asyncFunction/customerAsyncFunc";
+
+export const getCustomers = createAsyncThunk('customer/getCustomers',
+   async (arg, { getState, rejectWithValue }) => {
+      try {
+         const { data } = await getCustomersFunc()
+         return data.data
+      } catch (err) {
+         return rejectWithValue(err.response)
+      }
+   }
+)
+export const getCustomerById = createAsyncThunk('customer/getCustomerById',
+   async (id, { rejectWithValue }) => {
+      try {
+         const { data } = await getCustomerByIdFunc(id)
+         return data.data
+      } catch (err) {
+         return rejectWithValue(err.response)
+      }
+   }
+)
+export const addNewCustomer = createAsyncThunk('customer/addNewCustomer',
+   async (newData, { rejectWithValue }) => {
+      try {
+         const { first_name, last_name, mobile_number, age, dob } = newData
+
+         const posted = {
+            name: {
+               first_name: first_name,
+               last_name: last_name
+            },
+            phone_number: {
+               mobile_phone_number: mobile_number
+            },
+            age: age,
+            dob: dob
+         }
+         const config = {
+            headers: {
+               "Content-Type": "application/json"
+            }
+         }
+
+         const { data } = await addCustomerFunc(posted, config)
+         return data.data
+      } catch (err) {
+         return rejectWithValue(err.response)
+      }
+   }
+)
+
+
+export const deleteCustomer = createAsyncThunk('customer/deleteCustomer',
+   async (id, { rejectWithValue }) => {
+      try {
+         const { data } = await deleteCustomerFunc(id)
+         return data.data
+      } catch (err) {
+         return rejectWithValue(err.response)
+      }
+   }
+)
+
+export const updatedCustomer = createAsyncThunk('customer/updateCustomer',
+   async (receivedData, { rejectWithValue }) => {
+      try {
+         const { _id, first_name, last_name, mobile_number, age, dob } = receivedData
+
+         const posted = {
+            name: {
+               first_name: first_name,
+               last_name: last_name
+            },
+            phone_number: {
+               mobile_phone_number: mobile_number
+            },
+            age: age,
+            dob: dob
+         }
+         const config = {
+            headers: {
+               "Content-Type": "application/json"
+            }
+         }
+         const { data } = await updateCustomerFunc(_id, posted, config)
+         return data.data
+      } catch (err) {
+         return rejectWithValue(err.response)
+      }
+
+   }
+)
